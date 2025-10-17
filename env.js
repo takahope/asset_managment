db0fcd032b67ae5adb67d004b27711f39424bd96
biobankmacos <kolin.twbiobank@gmail.com>
2025-10-17 15:20:30 +0800

    init

---
+++ env.js
@@ -0,0 +1,5 @@
+const SPREADSHEET_ID = "1hzlVLPIqTYIzxczxczxczxcXaZwkG"; // 在試算表網址中可以找到
+
+// --- ✨ **新增**：報廢列印功能相關設定 ---
+const SCRAP_TEMPLATE_DOC_ID = "1Dta5j4Mt7oezxczxczxc1W7QaUM4y9Y"; // ⚠️ 請務必替換成您的範本ID
+const SCRAP_OUTPUT_FOLDER_ID = "1PqJwr_O2zxczxczxczxcbgF-r"; // 建議指定一個資料夾
